76587b27b0ede89b3fb13ac59f82d5e0b5560bf5
whileyou <59039974+whileyou@users.noreply.github.com>
2020-08-30 19:38:36 +0800

    Update dp算法总结.cpp

--- dp/dp算法总结.cpp
+++ dp/dp算法总结.cpp
@@ -9,4 +9,7 @@
 (2)当(1)方法不可行，考虑此方法。
 dp[i][j]:前i个数j个划分的解，且必须含第i个数。
 通过dp解最终问题的所有情况，用ans对每个dp[i][j]的解检测，得出最终解，即ans
+
+方式(1)参考：https://github.com/whileyou/important-algorithm/blob/master/dp/%E6%95%B0%E7%BB%84%E5%88%86%E5%89%B2%E6%88%90m%E6%AE%B5%E9%9D%9E%E7%A9%BA%E8%BF%9E%E7%BB%AD%E5%AD%90%E6%95%B0%E7%BB%84%EF%BC%8C%E5%85%B6%E5%90%84%E8%87%AA%E5%92%8C%E7%9A%84%E6%9C%80%E5%A4%A7%E5%80%BC%E7%9A%84%E6%9C%80%E5%B0%8F%E6%95%B0%E5%80%BC.cpp
+方式(2)参考：https://github.com/whileyou/important-algorithm/blob/master/dp/%E6%9C%80%E5%A4%A7m%E5%AD%90%E6%AE%B5%E5%92%8C.cpp
 */
